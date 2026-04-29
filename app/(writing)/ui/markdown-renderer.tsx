944cc1ccf74c4db3674e20fa16591a64634530f4
toyamarinyon <toyamarinyon@gmail.com>
2026-04-29 23:11:22 +0900

    Restore Streamdown code rendering in CSS

--- app/(writing)/ui/markdown-renderer.tsx
+++ app/(writing)/ui/markdown-renderer.tsx
@@ -9,23 +9,6 @@ export function MarkdownRenderer(props: {
 	return (
 		<Streamdown
 			className={props.className}
-			components={{
-				inlineCode: (inlineCodeProps) => {
-					// `node` is Streamdown AST metadata for custom renderers. Keep it out
-					// of the DOM or React will serialize it as `node="[object Object]"`.
-					// eslint-disable-next-line @typescript-eslint/no-unused-vars
-					const { children, className, node, ...codeProps } = inlineCodeProps;
-
-					return (
-						<code
-							{...codeProps}
-							className={`rounded-md bg-highlight-low px-1.5 py-0.5 font-medium text-sm leading-6 text-text ${className ?? ""}`}
-						>
-							{children}
-						</code>
-					);
-				},
-			}}
 			controls={{ code: { copy: true, download: false } }}
 			lineNumbers={false}
 		>

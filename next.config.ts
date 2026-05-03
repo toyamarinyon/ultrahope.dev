21484be684e09f54964c688171ffec15ac13dc0c
toyamarinyon <toyamarinyon@gmail.com>
2026-05-04 07:19:24 +0900

    Refine Google Cloud CLI install article

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	allowedDevOrigins: ['misepythongcloud.ultrahope-dev.localhost']
 };
 
 export default nextConfig;

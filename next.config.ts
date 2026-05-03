2482d698f94b19aa3c118e167097d3a0027507bf
toyamarinyon <toyamarinyon@gmail.com>
2026-05-04 07:20:30 +0900

    Allow wildcard ultrahope dev localhost origins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	allowedDevOrigins: ['misepythongcloud.ultrahope-dev.localhost']
+	allowedDevOrigins: ['*.ultrahope-dev.localhost']
 };
 
 export default nextConfig;
